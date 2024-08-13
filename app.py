7b053db684ca7bf05ee1a6d27f64dafe2c66c2d8
Manasvin5 <manasvinshrimali@gmail.com>
2024-08-14 01:13:58 +0530

    2nd Commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = request.form.get('num1')
+    num2 = request.form.get('num2')
+    operation = request.form.get('operation')
+
+    result = None
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            result = num1 / num2
+    except (ValueError, ZeroDivisionError):
+        result = 'Error'
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
